f60d29961dd8a7dc2f7f73476c03d4b190336347
Deepak-rock <deepakind777@gmail.com>
2024-07-14 20:25:32 +0530

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const Box = props => {
-  //  Write your code here.
+  const {sizeName, boxContainer} = props
+  return(
+    <div className = {boxContainer}>
+      <p className = "text">{sizeName}</p>
+    </div>
+  )
 }
 
 const element = (
-  //  Write your code here. 
+  <div className = "bg-container">
+    <h1 className = "heading">Boxes</h1>
+    <div className = "box-container">
+      <Box boxContainer = "small" sizeName = "Small"/>
+      <Box boxContainer = "medium" sizeName = "Medium"/>
+      <Box boxContainer = "large" sizeName = "Large"/>
+    </div>
+  </div> 
 )
 
 ReactDOM.render(element, document.getElementById("root"));
